3f05a84f970c4bc711c787c02629b1c81c938d68
Noah Wenig <nwenig@trinity.edu>
2024-10-13 15:06:13 -0500

    to do and comment for admin in sql file

--- setup.sql
+++ setup.sql
@@ -1,8 +1,14 @@
+
+
+--add admin bool to this 
+--always 0 for controller 
+--hard code in an admin user?
 CREATE TABLE users(
 userId SERIAL,
 username VARCHAR(15),
 password VARCHAR(15),
 email VARCHAR(99));
+
 
 
 
