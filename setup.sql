ccde522d40759b6dd262c8f60884b83fe72a508d
Noah Wenig <nwenig@trinity.edu>
2024-10-24 20:14:06 -0500

    passwords now hashed on creation

--- setup.sql
+++ setup.sql
@@ -5,9 +5,9 @@
 --hard code in an admin user?
 CREATE TABLE users (
   userID SERIAL PRIMARY KEY,
-  username VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
+  username VARCHAR(32) UNIQUE NOT NULL,
+  password VARCHAR(32) NOT NULL,
+  email VARCHAR(64) UNIQUE NOT NULL,
   is_admin BOOLEAN 
 );
 
