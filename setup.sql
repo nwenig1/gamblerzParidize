9a486b0e81364620ec0bdb6ecfa477df58994360
Theodore Skeen <linskeen@theodores-mbp.trinity.local>
2024-10-30 11:23:04 -0500

    database table setup

--- setup.sql
+++ setup.sql
@@ -8,7 +8,7 @@ CREATE TABLE users (
   username VARCHAR(32) UNIQUE NOT NULL,
   password VARCHAR(32) NOT NULL,
   email VARCHAR(64) UNIQUE NOT NULL,
-  is_admin BOOLEAN 
+  is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE products (
@@ -17,6 +17,37 @@ CREATE TABLE products (
   description VARCHAR(256) NOT NULL, 
   price NUMERIC(10, 2) NOT NULL
 );
+
+CREATE TABLE carts (
+  userID INTEGER NOT NULL,
+  productID INTEGER NOT NULL,
+  quantity INTEGER NOT NULL,
+  engravingText VARCHAR(64),
+  FOREIGN KEY (userID) REFERENCES users(userID),
+  FOREIGN KEY (productID) REFERENCES products(productID)
+);
+
+CREATE TABLE tags (
+  tagID SERIAL PRIMARY KEY,
+  tagText VARCHAR(255) NOT NULl
+);
+
+CREATE TABLE productTags (
+  productID INTEGER NOT NULL,
+  tagID INTEGER NOT NULL,
+  FOREIGN KEY (productID) REFERENCES products(productID),
+  FOREIGN KEY (tagID) REFERENCES tags(tagID)
+);
+
+CREATE TABLE images (
+  fileName VARCHAR(255) PRIMARY KEY,
+  altDescription VARCHAR(255),
+  productID INTEGER NOT NULL,
+  FOREIGN KEY (productID) REFERENCES products(productID)
+);
+
+
+
 --assuming product table created like schema 
 
 INSERT INTO products (name, description, price)
