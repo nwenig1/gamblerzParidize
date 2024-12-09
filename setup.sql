ce62965654f01ff184fd44dee9dbbfd24418768d
Kaelubagu <caguiar@trinity.edu>
2024-12-08 22:04:03 -0600

    finished stuff

--- setup.sql
+++ setup.sql
@@ -1,9 +1,6 @@
-
-
 --add admin bool to this 
 --always 0 for controller 
---hard code in an admin user? 
---actuallyt just making one normally than modifying it will be easier (hash will be weird if putting it in manually)
+--hard code in an admin user?
 CREATE TABLE users (
   userID SERIAL PRIMARY KEY,
   username VARCHAR(128) UNIQUE NOT NULL,
